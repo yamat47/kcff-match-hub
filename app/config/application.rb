eb8344e15c04822e3aac6bcf194f5d3905c9d99e
yamat47 <yamat47.thirddown@gmail.com>
2023-12-27 16:43:01 +0900

    Add RuboCop and RuboCop-Rails.

--- app/config/application.rb
+++ app/config/application.rb
@@ -1,16 +1,18 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails"
+require_relative 'boot'
+
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
@@ -19,6 +21,7 @@ require "action_view/railtie"
 Bundler.require(*Rails.groups)
 
 module KcffMatchHub
+  # rubocop:disable Style/Documentation
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
@@ -26,7 +29,7 @@ module KcffMatchHub
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -39,4 +42,5 @@ module KcffMatchHub
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
+  # rubocop:enable Style/Documentation
 end

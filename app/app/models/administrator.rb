3ccfd61c9097f66fe5036e15c9726542e99cf225
yamat47 <yamat47.thirddown@gmail.com>
2024-01-08 15:12:50 +0900

    Add administrator list.

--- app/app/models/administrator.rb
+++ app/app/models/administrator.rb
@@ -4,6 +4,8 @@ class Administrator < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
 
+  scope :name_ordered, -> { order(name: :asc) }
+
   # 8文字のランダムな英数字を生成してnameにセットする。
   # ただし既存のレコードと重複しないこともチェックする。
   # 重複していた場合は再度ランダムな英数字を生成してセットする。

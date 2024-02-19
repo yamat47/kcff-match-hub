47d5e1b5a97e662bfaf100a7c5e43f3feb073d65
yamat47 <yamat47.thirddown@gmail.com>
2024-02-20 07:56:08 +0900

    team CRUD.

--- app/app/models/team.rb
+++ app/app/models/team.rb
@@ -4,4 +4,12 @@ class Team < ApplicationRecord
   include UniversalId
 
   setup_universal_id('tem')
+  has_one_attached(:logo)
+
+  scope :search_by_keyword, lambda { |query|
+    where(<<-SQL.squish, query: "%#{query}%")
+      universal_id LIKE :query
+      OR name LIKE :query
+    SQL
+  }
 end

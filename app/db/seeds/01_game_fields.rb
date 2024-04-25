112d3c73229837ca3212264e9a6173d643d9bc68
yamat47 <yamat47.thirddown@gmail.com>
2024-04-25 09:54:26 +0900

    Add game_fields seed.

---
+++ app/db/seeds/01_game_fields.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'factory_bot'
+
+FactoryBot.create(
+  :game_field,
+  name: '東京大学 検見川総合運動場',
+  address: '千葉県千葉市花見川区花園町1035',
+  google_maps_url: 'https://maps.app.goo.gl/ohkqyavD7zWGK82a6',
+  google_maps_embed_src_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6483.4076962118515!2d140.0752947!3d35.6596675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x602283decc0e3c1f%3A0x4a6782f1e1d462cf!2z5p2x5Lqs5aSn5a2m5qSc6KaL5bed57eP5ZCI6YGL5YuV5aC0!5e0!3m2!1sja!2sjp!4v1714005029732!5m2!1sja!2sjp'
+)
+
+FactoryBot.create(
+  :game_field,
+  name: '東洋大学 川越キャンパス Hグラウンド',
+  address: '埼玉県川越市鯨井2100',
+  google_maps_url: 'https://maps.app.goo.gl/gnzueshKoeiM9Tuk9',
+  google_maps_embed_src_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3230.5760673059203!2d139.43111383285742!3d35.932875386176995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018d7477ba30a37%3A0x689499a32244d7e4!2z5p2x5rSL5aSn5a2mIOW3nei2iuOCreODo-ODs-ODkeOCuQ!5e0!3m2!1sja!2sjp!4v1714005528918!5m2!1sja!2sjp'
+)
+
+FactoryBot.create(
+  :game_field,
+  name: '慶應義塾大学 矢上キャンパス',
+  address: '神奈川県横浜市港北区日吉3-14-1',
+  google_maps_url: 'https://maps.app.goo.gl/cRs8GU7VT3NcMBgUA',
+  google_maps_embed_src_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2446.6278998737434!2d139.65411795717293!3d35.5546610727524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60185f9c5d6e11e5%3A0x2781d6958786ab3a!2z5oW25oeJ576p5aG-5aSn5a2mIOefouS4iuOCreODo-ODs-ODkeOCuQ!5e0!3m2!1sja!2sjp!4v1714005627714!5m2!1sja!2sjp'
+)
+
+FactoryBot.create(
+  :game_field,
+  name: '駒沢公園 第二球技場',
+  address: '東京都世田谷区１ 駒沢5-2-10',
+  google_maps_url: 'https://maps.app.goo.gl/scos58tXQkA6z1YZ7',
+  google_maps_embed_src_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6486.0952078871305!2d139.65740487569786!3d35.62655293297573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f48d0d4ea671%3A0x66f9e9baa1584e91!2z6aeS5rKi5YWs5ZySIOesrOS6jOeQg-aKgOWgtA!5e0!3m2!1sja!2sjp!4v1714005942725!5m2!1sja!2sjp'
+)
+
+FactoryBot.create(
+  :game_field,
+  name: '日本大学理工学部 船橋キャンパス',
+  address: '千葉県船橋市習志野台7-20',
+  google_maps_url: 'https://maps.app.goo.gl/7cxp8vXkyArQ2ouXA',
+  google_maps_embed_src_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1619.4714144859975!2d140.05407410976952!3d35.727624540025985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x602281230ff99195%3A0x448363d7e7e5368b!2z5pel5pys5aSn5a2m55CG5bel5a2m6YOo6Ii55qmL44Kt44Oj44Oz44OR44K544Kw44Op44Km44Oz44OJ!5e0!3m2!1sja!2sjp!4v1714006431842!5m2!1sja!2sjp'
+)

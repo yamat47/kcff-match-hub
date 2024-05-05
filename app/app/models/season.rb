e9b658f61da89e48393c5c8815c861128682786f
yamat47 <yamat47.thirddown@gmail.com>
2024-05-05 13:26:09 +0900

    Add seasons CRUD.

--- app/app/models/season.rb
+++ app/app/models/season.rb
@@ -9,4 +9,14 @@ class Season < ApplicationRecord
 
   validates :full_name, presence: true, uniqueness: true
   validates :short_name, presence: true, uniqueness: true
+
+  scope :search_by_keyword, lambda { |query|
+    where(<<-SQL.squish, query: "%#{query}%")
+      universal_id LIKE :query
+      OR full_name LIKE :query
+      OR short_name LIKE :query
+    SQL
+  }
+
+  scope :order_by_full_name, -> { order(:full_name) }
 end

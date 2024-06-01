390954059efc55d01eb75691ecdb292a466410e6
yamat47 <yamat47.thirddown@gmail.com>
2024-06-01 21:52:57 +0900

    Game schedules list.

--- app/app/controllers/media/game_schedules_controller.rb
+++ app/app/controllers/media/game_schedules_controller.rb
@@ -3,6 +3,13 @@
 module Media
   class GameSchedulesController < ApplicationController
     def index
+      # FIXME: filter by season.
+      # FIXME: order by game_field.
+      game_schedules = GameSchedule.start_at_ordered
+                                   .preload(:home_team, :visitor_team, :game_field, :tournament)
+
+      # group by date.
+      @game_schedule_groups = game_schedules.group_by { |game_schedule| game_schedule.start_at.to_date }
     end
   end
 end
--- app/app/controllers/media/teams_controller.rb
+++ app/app/controllers/media/teams_controller.rb
@@ -4,6 +4,7 @@ module Media
   class TeamsController < ApplicationController
     def index
       @teams = Team.order_by_name
+                   .preload({ logo_attachment: :blob }, :team_profile)
     end
   end
 end

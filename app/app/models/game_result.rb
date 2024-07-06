a2270cd28217206f81d96eca41479d9720e4a305
yamat47 <yamat47.thirddown@gmail.com>
2024-07-06 17:57:59 +0900

    Update validation error message.

--- app/app/models/game_result.rb
+++ app/app/models/game_result.rb
@@ -21,19 +21,19 @@ class GameResult < ApplicationRecord
   private
 
   def home_win_score_must_be_higher
-    errors.add(:result, :invalid) unless home_team_score > visitor_team_score
+    errors.add(:result, :inconsistent) unless home_team_score > visitor_team_score
   end
 
   def visitor_win_score_must_be_lower
-    errors.add(:result, :invalid) unless home_team_score < visitor_team_score
+    errors.add(:result, :inconsistent) unless home_team_score < visitor_team_score
   end
 
   def draw_scores_must_be_equal
-    errors.add(:result, :invalid) unless home_team_score == visitor_team_score
+    errors.add(:result, :inconsistent) unless home_team_score == visitor_team_score
   end
 
   def canceled_scores_must_be_zero
-    # The scores must be zero when the game is canceled.
-    errors.add(:result, :invalid) unless home_team_score.zero? && visitor_team_score.zero?
+    errors.add(:home_team_score, :nonzero) unless home_team_score.zero?
+    errors.add(:visitor_team_score, :nonzero) unless visitor_team_score.zero?
   end
 end

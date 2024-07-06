2f8aaf0e797066dbc33bbca28557940d64117d75
yamat47 <yamat47.thirddown@gmail.com>
2024-07-06 16:20:46 +0900

    Add game_result seeds.

--- app/spec/factories/game_results.rb
+++ app/spec/factories/game_results.rb
@@ -17,5 +17,29 @@ FactoryBot.define do
         :draw
       end
     end
+
+    trait :home_win do
+      home_team_score { rand(11..20) }
+      visitor_team_score { rand(0..10) }
+      result { :home_win }
+    end
+
+    trait :visitor_win do
+      home_team_score { rand(0..10) }
+      visitor_team_score { rand(11..20) }
+      result { :visitor_win }
+    end
+
+    trait :draw do
+      home_team_score { rand(0..10) }
+      visitor_team_score { home_team_score }
+      result { :draw }
+    end
+
+    trait :canceled do
+      home_team_score { 0 }
+      visitor_team_score { 0 }
+      result { :canceled }
+    end
   end
 end

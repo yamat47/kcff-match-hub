bc8015d5c582368ace778abdabc0e8d02185b911
yamat47 <yamat47.thirddown@gmail.com>
2024-07-15 23:31:38 +0900

    Use AWS S3 for Active Storage in production.

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -35,7 +35,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
   # Print deprecation notices to the Rails logger.
